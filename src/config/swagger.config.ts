329449a4984bce6b867fd97ab0e867a6059fbd25
ghnoob <ptnchk.rodrigo@gmail.com>
2022-04-20 19:47:19 -0300

    add swagger config object

---
+++ src/config/swagger.config.ts
@@ -0,0 +1,29 @@
+import { join } from 'path';
+import { Options } from 'swagger-jsdoc';
+
+/**
+ * Swagger options.
+ */
+const swaggerConfig: Options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'movies-api',
+      version: '1.0.0',
+      description: "REST API made for Alkemy's Node.js Backend Challenge.",
+      license: {
+        name: 'GPL 3.0',
+        url: 'https://www.gnu.org/licenses/gpl-3.0.txt',
+      },
+    },
+  },
+  apis: [
+    join(__dirname, '..', 'controllers', 'controllers', '*.controller.ts'),
+    join(__dirname, '..', 'errors', '*.error.ts'),
+    join(__dirname, '..', 'models', 'models', '*.model.ts'),
+    join(__dirname, '..', 'models', 'dto', '*.dto.ts'),
+    join(__dirname, '..', 'routes', '*.router.ts'),
+  ],
+};
+
+export default swaggerConfig;
