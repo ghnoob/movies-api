8fcce89892a714517a28c22902feba4ce5e5b1c3
ghnoob <ptnchk.rodrigo@gmail.com>
2022-04-23 01:05:00 -0300

    add migration for characters

---
+++ src/database/migrations/20220423035219-CharactersTable.ts
@@ -0,0 +1,30 @@
+import { DataTypes, QueryInterface } from 'sequelize';
+
+const migration = {
+  async up(queryInterface: QueryInterface) {
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.createTable('characters', {
+        id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+          autoIncrementIdentity: true,
+        },
+        name: DataTypes.STRING(30),
+        imageUrl: DataTypes.STRING(2048),
+        age: DataTypes.INTEGER,
+        weight: DataTypes.INTEGER,
+        history: DataTypes.STRING(1000),
+      }, { transaction });
+    });
+  },
+
+  async down(queryInterface: QueryInterface) {
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.dropTable('characters', { transaction });
+    });
+  }
+};
+
+export default migration;
