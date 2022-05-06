3fc20f8982209cc5b267b089c1f4dd6d46a495ea
ghnoob <ptnchk.rodrigo@gmail.com>
2022-05-06 00:40:53 -0300

    add jwt key env variable

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -4,6 +4,7 @@
 const appConfig = {
   ENVIRONMENT: process.env.NODE_ENV || 'development',
   PORT: process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 3000,
+  JWT_SECRET_KEY: process.env.JWT_SECRET_KEY || 'default',
 };
 
 export default appConfig;
