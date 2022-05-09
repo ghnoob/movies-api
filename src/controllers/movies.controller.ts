82428ebbca588947a608dd240124f99c6aa4c56c
ghnoob <ptnchk.rodrigo@gmail.com>
2022-05-09 20:37:15 -0300

    add method to get movie by id

--- src/controllers/movies.controller.ts
+++ src/controllers/movies.controller.ts
@@ -1,6 +1,7 @@
 import { instanceToPlain } from 'class-transformer';
 import { Request, Response, NextFunction } from 'express';
 import { Service } from 'typedi';
+import HttpError from '../errors/http.error';
 import HttpStatus from '../models/enums/http-status.enum';
 import MoviesService from './services/movies.service';
 
@@ -22,6 +23,29 @@ export default class MoviesController {
       return res
         .status(HttpStatus.OK)
         .json(instanceToPlain(result, { excludeExtraneousValues: true }));
+    } catch (err) {
+      return next(err);
+    }
+  }
+
+  /**
+   * Returns the details of a movie.
+   */
+  async findOne(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<Response | void> {
+    try {
+      const movie = await this.service.findOne(Number(req.params.id));
+
+      if (movie) {
+        return res
+          .status(HttpStatus.OK)
+          .json(instanceToPlain(movie, { excludeExtraneousValues: true }));
+      }
+
+      return next(new HttpError(HttpStatus.NOT_FOUND, 'Movie not found.'));
     } catch (err) {
       return next(err);
     }
