fd66a530b3b2b9af14158d8bca75a031a160e1d7
ghnoob <ptnchk.rodrigo@gmail.com>
2022-05-09 22:46:46 -0300

    add endpoint to create movies

--- src/routes/movies.routes.ts
+++ src/routes/movies.routes.ts
@@ -1,6 +1,8 @@
 import { Service } from 'typedi';
 import MoviesController from '../controllers/movies.controller';
+import authenticateJwt from '../middlewares/authenticate-jwt.middleware';
 import validateRequest from '../middlewares/validate-request.middleware';
+import CreateMovieDto from '../models/dto/movies/create-movie.dto';
 import IdParamDto from '../models/dto/id-param.dto';
 import CommonRoutes from './common.routes';
 
@@ -18,6 +20,13 @@ export default class MoviesRoutes extends CommonRoutes {
       '/:id',
       validateRequest(IdParamDto, 'params'),
       this.controller.findOne.bind(this.controller),
+    );
+
+    this.router.post(
+      '/',
+      authenticateJwt,
+      validateRequest(CreateMovieDto),
+      this.controller.create.bind(this.controller),
     );
   }
 }
