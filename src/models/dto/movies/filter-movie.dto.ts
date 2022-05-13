5684b1e98298df5932f74c620a1b4405144097fe
ghnoob <ptnchk.rodrigo@gmail.com>
2022-05-12 22:38:38 -0300

    add dto to sanitize movie filters

---
+++ src/models/dto/movies/filter-movie.dto.ts
@@ -0,0 +1,49 @@
+import { Expose, Transform } from 'class-transformer';
+import { IsOptional } from 'class-validator';
+import NormalizeQueryParamString from '../../../decorators/normalize-query-param-string.decorator';
+import Trim from '../../../decorators/trim.decorator';
+import PaginateDto from '../paginate.dto';
+
+/**
+ * Movies filter options.
+ */
+export default class FilterMovieDto extends PaginateDto {
+  /**
+   * Movie's title.
+   */
+  @Expose()
+  @IsOptional()
+  @Trim()
+  @NormalizeQueryParamString()
+  title?: string;
+
+  /**
+   * Id of the movie's genre
+   */
+  @Expose()
+  @IsOptional()
+  @Transform(({ value }) => {
+    if (value == null) {
+      return value;
+    }
+
+    if (Array.isArray(value)) {
+      value = value[0];
+    }
+
+    return !isNaN(value) ? Number(value) : null;
+  })
+  genre?: number | null;
+
+  /**
+   * Direction to sort the movie's creation date by.
+   */
+  @Expose()
+  @IsOptional()
+  @Transform(({ value }) =>
+    String(value).toUpperCase() === 'DESC' ? 'DESC' : 'ASC',
+  )
+  @Trim()
+  @NormalizeQueryParamString()
+  order: 'ASC' | 'DESC' = 'ASC';
+}
