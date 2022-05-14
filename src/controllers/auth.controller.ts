69fba2bbbaffb552303b748a01eefd7cc0e70314
ghnoob <ptnchk.rodrigo@gmail.com>
2022-05-14 16:13:42 -0300

    docs: auth responses

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,3 +1,41 @@
+/**
+ * @swagger
+ * components:
+ *   responses:
+ *     UserRegistered:
+ *       description: The account was registered successfully
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               user:
+ *                 $ref: '#/components/schemas/User'
+ *
+ *     UserLoggedIn:
+ *       description: User logged in successfully.
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               token:
+ *                 type: string
+ *                 description: The generated JWT for the user.
+ *                 example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJ1c2VyQGV4YW1wbGUuY29tIiwiaWF0IjoxNTE2MjM5MDIyfQ.gzAorqvnGloYu17fanX-yhDQlpQK18C5M64gkcmhrqQ
+ *
+ *     UserAlreadyExists:
+ *       description: An user with the provided email already exists.
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/ErrorResponse'
+ *           example:
+ *             statusCode: 409
+ *             name: Conflict
+ *             message: Email is already in use.
+ */
+
 import { Request, Response, NextFunction } from 'express';
 import { sign } from 'jsonwebtoken';
 import { Service } from 'typedi';
@@ -9,10 +47,16 @@ import HttpStatus from '../models/enums/http-status.enum';
  */
 @Service()
 export default class AuthController {
+  /**
+   * Creates new account.
+   */
   async register(req: Request, res: Response): Promise<Response> {
     return res.status(HttpStatus.CREATED).json({ user: req.user });
   }
 
+  /**
+   * Logs in to a created account.
+   */
   async login(
     req: Request,
     res: Response,
