c84b00be243d952a11bef82c5c645d26f01e8950
ghnoob <ptnchk.rodrigo@gmail.com>
2022-05-14 23:52:49 -0300

    docs: get all characters

--- src/routes/characters.routes.ts
+++ src/routes/characters.routes.ts
@@ -1,3 +1,50 @@
+/**
+ * @swagger
+ * paths:
+ *   /characters:
+ *     get:
+ *       tags:
+ *         - characters
+ *       summary: Get all characters
+ *       parameters:
+ *         - in: query
+ *           name: name
+ *           schema:
+ *             type: string
+ *           description: Filter by name
+ *         - in: query
+ *           name: age
+ *           schema:
+ *             oneOf:
+ *               - type: integer
+ *               - $ref: '#/components/schemas/NumericFilter'
+ *           description: >
+ *             Filter by age. Can be an integer to just search for characters whose age equals the
+ *             input value or an object as indicated in the `NumericFilter` schema to specify ranges.
+ *         - in: query
+ *           name: weight
+ *           schema:
+ *             oneOf:
+ *               - type: integer
+ *               - $ref: '#/components/schemas/NumericFilter'
+ *           description: >
+ *             Filter by weight. Can be an integer to just search for characters whose weight equals the
+ *             input value or an object as indicated in the `NumericFilter` schema to specify ranges.
+ *         - in: query
+ *           name: movies
+ *           schema:
+ *             type: array
+ *             items:
+ *               type: integer
+ *           description: >
+ *             Filter by movies (using their ids) where the characters appeared.
+ *             If more than one movie id is supplied, it will select characters who appeared
+ *             in at least one of those movies
+ *       responses:
+ *         200:
+ *           $ref: '#/components/responses/CharacterList'
+ */
+
 import { Service } from 'typedi';
 import authenticateJwt from '../middlewares/authenticate-jwt.middleware';
 import validateRequest from '../middlewares/validate-request.middleware';
