55ab35a350823ed83d174080915cc01663c22505
ghnoob <ptnchk.rodrigo@gmail.com>
2022-05-14 23:29:22 -0300

    docs: pagination result schema

--- src/routes/common.routes.ts
+++ src/routes/common.routes.ts
@@ -1,3 +1,41 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     PaginationResult:
+ *       type: object
+ *       required:
+ *         - data
+ *         - totalPages
+ *         - total
+ *       properties:
+ *         data:
+ *           type: array
+ *           items:
+ *             type: object
+ *           description: List of items in the current page.
+ *         totalPages:
+ *           type: integer
+ *           minimum: 0
+ *           example: 1
+ *           description: Total pages in the current query.
+ *         total:
+ *           type: integer
+ *           minimum: 0
+ *           example: 1
+ *           description: Total items in the current query
+ *         previous:
+ *           type: string
+ *           format: url
+ *           example: '/resource?limit=1&page=1'
+ *           description: Link to previous page
+ *         next:
+ *           type: string
+ *           format: url
+ *           example: '/resource?limit=1&page=3'
+ *           description: Link to next page
+ */
+
 import { Router } from 'express';
 
 /**
