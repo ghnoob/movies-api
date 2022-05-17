7f1015c760e84b7226c33db34e1557cb5386007b
ghnoob <ptnchk.rodrigo@gmail.com>
2022-05-17 20:50:24 -0300

    docs: movie created response

--- src/controllers/movies.controller.ts
+++ src/controllers/movies.controller.ts
@@ -14,6 +14,18 @@
  *                   data:
  *                     items:
  *                       $ref: '#/components/schemas/Movie'
+ *     MovieCreated:
+ *       description: Movie created successfully.
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               id:
+ *                 type: integer
+ *                 readOnly: true
+ *                 description: Id of the created movie.
+ *                 example: 1
  */
 
 import { instanceToPlain } from 'class-transformer';
