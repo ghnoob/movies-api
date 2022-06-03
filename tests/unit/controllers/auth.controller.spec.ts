c58d76960c0363f2dae9c725e695d3d0fa2563db
ghnoob <ptnchk.rodrigo@gmail.com>
2022-06-03 01:26:15 -0300

    test: auth controller

---
+++ tests/unit/controllers/auth.controller.spec.ts
@@ -0,0 +1,76 @@
+import { expect, use } from 'chai';
+import { noCallThru } from 'proxyquire';
+import { stub } from 'sinon';
+import sinonChai from 'sinon-chai';
+import { mockReq, mockRes } from 'sinon-express-mock';
+import AuthController from '../../../src/controllers/auth.controller';
+import HttpStatus from '../../../src/models/enums/http-status.enum';
+
+const proxyquire = noCallThru();
+
+use(sinonChai);
+
+describe('auth controller tests', () => {
+  const mockToken = { id: 1, email: 'user@domain.com' };
+
+  const req = mockReq({ user: mockToken, app: { emit: stub() } }),
+    res = mockRes(),
+    next = stub();
+
+  afterEach(() => {
+    res.status.resetHistory();
+    res.json.resetHistory();
+    next.resetHistory();
+  });
+
+  describe('register', () => {
+    const controller = new AuthController();
+
+    it('should emit an event an return correct response', async () => {
+      await controller.register(req, res);
+
+      expect(req.app.emit).to.have.been.calledOnceWithExactly(
+        'event:userCreated',
+        mockToken.email,
+      );
+      expect(res.status).to.have.been.calledOnceWithExactly(HttpStatus.CREATED);
+      expect(res.json).to.have.been.calledOnceWithExactly({ user: mockToken });
+    });
+  });
+
+  describe('login', () => {
+    const mockSign = stub();
+
+    const AuthControllerStubbed = proxyquire(
+      '../../../src/controllers/auth.controller',
+      {
+        jsonwebtoken: { sign: mockSign },
+      },
+    ).default;
+
+    const controller: AuthController = new AuthControllerStubbed();
+
+    afterEach(() => {
+      mockSign.reset();
+    });
+
+    it('should return the signed token', async () => {
+      mockSign.resolves('test');
+
+      await controller.login(req, res, next);
+
+      expect(res.status).to.have.been.calledOnceWithExactly(HttpStatus.OK);
+      expect(res.json).to.have.been.calledOnceWithExactly({ token: 'test' });
+    });
+
+    it('should call next', async () => {
+      const err = new Error();
+
+      mockSign.rejects(err);
+
+      await controller.login(req, res, next);
+
+      expect(next).to.have.been.calledOnceWithExactly(err);
+    });
+  });
+});
