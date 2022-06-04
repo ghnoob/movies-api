bc90b7c7ad9e2ea8bc11e2f9722bf5cda9ab3e52
ghnoob <ptnchk.rodrigo@gmail.com>
2022-06-03 23:00:11 -0300

    test: controller findOne method

--- tests/unit/controllers/characters.controller.spec.ts
+++ tests/unit/controllers/characters.controller.spec.ts
@@ -1,10 +1,12 @@
 import { expect, use } from 'chai';
-import { createStubInstance, SinonStubbedInstance, stub } from 'sinon';
+import { createStubInstance, match, SinonStubbedInstance, stub } from 'sinon';
 import sinonChai from 'sinon-chai';
 import { mockReq, mockRes } from 'sinon-express-mock';
+import Character from '../../../src/models/character.model';
 import CharactersController from '../../../src/controllers/characters.controller';
 import CharactersService from '../../../src/controllers/services/characters.service';
 import HttpStatus from '../../../src/models/enums/http-status.enum';
+import HttpError from '../../../src/errors/http.error';
 
 use(sinonChai);
 
@@ -51,6 +53,56 @@ describe('characters controller tests', () => {
       service.findAll.rejects(err);
 
       await controller.findAll(req, res, next);
+
+      expect(next).to.have.been.calledOnceWithExactly(err);
+    });
+  });
+
+  describe('findOne', () => {
+    const params = { id: '1' };
+    const req = mockReq({ params });
+
+    afterEach(() => {
+      service.findOne.reset();
+    });
+
+    it('should return a character', async () => {
+      const character = createStubInstance(Character);
+      character.id = 1;
+      character.name = 'Simba';
+      character.movies = [];
+
+      service.findOne.resolves(character);
+
+      await controller.findOne(req, res, next);
+
+      expect(service.findOne).to.have.been.calledOnceWithExactly(1);
+      expect(res.status).to.have.been.calledOnceWithExactly(HttpStatus.OK);
+      expect(res.json).callCount(1);
+      expect(res.json).to.have.been.calledWithMatch({
+        id: 1,
+        name: 'Simba',
+        movies: [],
+      });
+    });
+
+    it('should call next with 404 error', async () => {
+      service.findOne.resolves(null);
+
+      await controller.findOne(req, res, next);
+
+      expect(next).to.have.been.calledOnceWithExactly(
+        match
+          .instanceOf(HttpError)
+          .and(match.has('status', HttpStatus.NOT_FOUND)),
+      );
+    });
+
+    it('should call next with error', async () => {
+      const err = new Error();
+      service.findOne.rejects(err);
+
+      await controller.findOne(req, res, next);
 
       expect(next).to.have.been.calledOnceWithExactly(err);
     });
