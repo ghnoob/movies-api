1ddf7274a2d380666988028194b023306c9f8390
ghnoob <ptnchk.rodrigo@gmail.com>
2022-06-07 16:25:32 -0300

    test: check that an empty object is passed on undefined body

--- tests/unit/middlewares/validate-request.middleware.spec.ts
+++ tests/unit/middlewares/validate-request.middleware.spec.ts
@@ -4,6 +4,7 @@ import sinonChai from 'sinon-chai';
 import { mockReq, mockRes } from 'sinon-express-mock';
 import { Expose } from 'class-transformer';
 import { IsString } from 'class-validator';
+import proxyquire from 'proxyquire';
 import validateRequest from '../../../src/middlewares/validate-request.middleware';
 import HttpError from '../../../src/errors/http.error';
 import HttpStatus from '../../../src/models/enums/http-status.enum';
@@ -43,5 +44,24 @@ describe('validate request middleware tests', () => {
         .instanceOf(HttpError)
         .and(match.has('status', HttpStatus.BAD_REQUEST)),
     );
+  });
+
+  it('should pass empty object on undefined body', async () => {
+    const req = mockReq({ body: undefined });
+
+    const plainToInstanceStub = stub().returns(new ExampleClass()),
+      spiedMiddleware = proxyquire(
+        '../../../src/middlewares/validate-request.middleware',
+        {
+          'class-transformer': {
+            plainToInstance: plainToInstanceStub,
+          },
+        },
+      ).default;
+
+    await spiedMiddleware(ExampleClass)(req, res, next);
+
+    expect(plainToInstanceStub).to.have.been.calledOnce;
+    expect(plainToInstanceStub.lastCall.args[1]).to.deep.equal({});
   });
 });
