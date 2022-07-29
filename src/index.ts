7a79d34f47db685857af8b37348e335b0b597a55
ghnoob <ptnchk.rodrigo@gmail.com>
2022-07-29 13:14:04 -0300

    simple entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,31 @@
 import 'reflect-metadata';
 import 'dotenv/config';
-import bootstrap from './bootstrap';
+import { Container } from 'typedi';
+import DbConnection from './database/connection';
+import app from './express';
+import appConfig from './config/app.config';
+import logger from './logger';
+import Seeder from './database/seeder';
 
-bootstrap();
+async function bootstrap() {
+  const db = Container.get(DbConnection).getConnection();
+
+  await db.authenticate();
+
+  if (appConfig.ENVIRONMENT !== 'production') {
+    await db.getQueryInterface().dropAllTables();
+    await db.sync();
+
+    await Container.get(Seeder).initialize();
+  }
+
+  logger.info('Database connected.');
+
+  app.listen(appConfig.PORT, () => {
+    logger.info(`App listening on port ${appConfig.PORT}`);
+  });
+}
+
+if (require.main === module) {
+  bootstrap();
+}
